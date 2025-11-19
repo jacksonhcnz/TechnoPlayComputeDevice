b30a7ce70a89a0aae18546d3c524277c87fe5c74
jacksonhcnz <jackson.hansen.cane@gmail.com>
2025-11-20 12:37:37 +1300

    Update setupfile.sh
    added keyboard to pip modules

--- setupfile.sh
+++ setupfile.sh
@@ -7,6 +7,6 @@ echo "Installing Python 3 and pip..."
 sudo apt install -y python3 python3-pip
 
 echo "Installing Python packages..."
-pip3 install --break-system-packages textual pygame RPi.GPIO
+pip3 install --break-system-packages textual pygame RPi.GPIO keyboard
 
 echo "Installation complete!"
