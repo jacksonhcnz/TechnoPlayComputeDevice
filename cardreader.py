835db97b76c49f77badba9dac345dff6fda341c9
jacksonhcnz <jackson.hansen.cane@gmail.com>
2025-11-21 12:19:23 +1300

    Update cardreader.py

--- cardreader.py
+++ cardreader.py
@@ -1,13 +1,13 @@
-import RPi.GPIO as GPIO
+         import RPi.GPIO as GPIO
 import time
 
 # Example: use pins 17, 27, 22, 23 (4-bit number)
-PINS = [17, 27, 22, 23]
+PINS = [17, 27, 22, 23, 24]
 
 def setup():
     GPIO.setmode(GPIO.BCM)   # use Broadcom pin numbering
     for pin in PINS:
-        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(pin, GPIO.out, pull_up_down=GPIO.PUD_DOWN)
 
 def read_binary_to_decimal():
     value = 0
